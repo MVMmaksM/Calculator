a6cb940ab6e7c88794abc7931bde5bb24120b62b
MVMmaksM <vaganov.maksim94@yandex.ru>
2023-04-05 10:28:47 +0500

    Добавьте файлы проекта.

---
+++ Calculator.Tests/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    internal class CalculatorTest
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue() 
+        { 
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Additional(10, 20) == 30);
+        }
+
+        [Test]
+        public void SubstractionReturnsCorrectValue() 
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Substraction(300, 20) == 280);
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public int Additional(int a, int b) => a + b;
+        public int Substraction(int a, int b) => a - b;
+        public int Multiplication(int a, int b) => a * b;
+        public int Division(int a, int b)=> a / b;
+    }
+}
